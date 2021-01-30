f364f9eacddee5c3525ab5ca70b87be9d84d0185
Kyle Asaff <kyleasaff@pixleeteam.com>
2021-01-30 14:55:44 -0400

    without react native offline

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
-export default function App() {
+import {
+  selectMapRoad, unselectMapRoad,
+} from './store/actions/map';
+
+const Example = () => {
+  const dispatch = useDispatch();
+  const onSelectMapRoad = () => {
+    dispatch(selectMapRoad(1));
+  }
   return (
+    <Provider store={store}>
     <View style={styles.container}>
+      <TouchableOpacity onPress={onSelectMapRoad}><Text>dispatch</Text></TouchableOpacity>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
+  );
+}
+
+export default function App() {
+  return (
+    <Provider store={store}>
+    <View style={styles.container}>
+    <Example />
+    </View>
+    </Provider>
   );
 }
 
